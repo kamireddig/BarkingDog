642643f277525879d6bb3bb84066414facfcf455
Pratik <pillu772845@gmail.com>
2020-03-04 21:27:17 -0600

    Initial Commit

---
+++ src/com/javaLearning/BarkingDog.java
@@ -0,0 +1,21 @@
+package com.javaLearning;
+
+public class BarkingDog {
+    public static void main(String[] args){
+        shouldWakeUp(true, 1);
+        shouldWakeUp(false, 2);
+        shouldWakeUp(true, 8);
+        shouldWakeUp(true, -1);
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if (barking)
+            if(hourOfDay < 0 || hourOfDay > 23)
+                return false;
+            else if(hourOfDay < 8 || hourOfDay > 22)
+                return true;
+            else
+                return false;
+        else
+            return false;
+    }
+}
